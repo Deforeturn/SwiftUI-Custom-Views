71bf397cb19a7733cfbca305769b86720ea5978e
JiHoon K <deferare@icloud.com>
2024-06-24 01:26:35 +0900

    Create temporal tab content views

---
+++ CustomViews/CustomViews/CustomTabBar/Temp+Views.swift
@@ -0,0 +1,36 @@
+//
+//  HomeView.swift
+//  CustomViews
+//
+//  Created by JiHoon K on 6/24/24.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text("Home")
+    }
+}
+
+struct SearchView: View {
+    var body: some View {
+        Text("Search")
+    }
+}
+
+struct FavoriteView: View {
+    var body: some View {
+        Text("Favorite")
+    }
+}
+
+struct ProfileView: View {
+    var body: some View {
+        Text("Profile")
+    }
+}
+
+#Preview {
+    HomeView()
+}
